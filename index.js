c0bcd7327ba9a82e82e38ba341ed84a744fc46cf
Tamakuz <jovangithub@gmail.com>
2023-08-03 18:14:16 +0700

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-import jsonServer from "json-server"
+import jsonServer from "json-server";
+import cors from "cors";
 
-const server = jsonServer.create()
-const router = jsonServer.router("datas.json")
-const middlewares = jsonServer.defaults()
-const port = 5000
+const server = jsonServer.create();
+const router = jsonServer.router("datas.json");
+const middlewares = jsonServer.defaults();
+const port = 5000;
 
-server.use(middlewares)
-server.use(router)
-server.listen(port)
+const allowedOrigins = [
+  "http://localhost:5173",
+];
+
+const customCorsOptions = {
+  origin: allowedOrigins,
+};
+
+server.use(cors(customCorsOptions)); // Add the CORS middleware with custom options
+server.use(middlewares);
+server.use(router);
+
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
