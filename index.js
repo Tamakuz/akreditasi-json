a53868392ae2faa7c12d8bd7ff9f08f4b0e2ac1f
Tamakuz <jovangithub@gmail.com>
2023-08-03 17:40:00 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import jsonServer from "json-server"
+
+const server = jsonServer.create()
+const router = jsonServer.router("datas.json")
+const middlewares = jsonServer.defaults()
+const port = 5000
+
+server.use(middlewares)
+server.use(router)
+server.listen(port)
