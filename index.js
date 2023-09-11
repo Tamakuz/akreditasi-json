b499c04a01dd869d8a1bed3daf1b0c7d27e66d08
Tamakuz <jovangithub@gmail.com>
2023-09-11 11:45:29 +0700

    new config 8

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const customCorsOptions = {
   origin: allowedOrigins,
 };
 
-server.use(cors(customCorsOptions)); // Add the CORS middleware with custom options
+server.use(cors()); // Add the CORS middleware with custom options
 server.use(middlewares);
 server.use(router);
 
