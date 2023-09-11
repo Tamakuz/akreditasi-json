b8385997f6562788cf3561e917a5f56a6aeda222
Tamakuz <jovangithub@gmail.com>
2023-09-11 10:11:11 +0700

    new config

--- index.js
+++ index.js
@@ -14,9 +14,41 @@ const customCorsOptions = {
   origin: allowedOrigins,
 };
 
-server.use(cors(customCorsOptions)); // Add the CORS middleware with custom options
+server.use(cors(customCorsOptions));
 server.use(middlewares);
 server.use(router);
+
+server.post("/:resource", (req, res) => {
+  const { resource } = req.params;
+  const db = router.db;
+
+  if (db.has(resource).value()) {
+    const data = req.body;
+    data.id = generateId(db.get(resource).value()); // Generate a unique ID
+    db.get(resource).push(data).write();
+    res.status(201).json({ message: "Data added successfully", data });
+  } else {
+    res.status(404).json({ error: "Resource not found" });
+  }
+});
+
+server.delete("/:resource/:id", (req, res) => {
+  const { resource, id } = req.params;
+  const db = router.db;
+
+  if (db.has(resource).value()) {
+    const index = db.get(resource).value().findIndex((item) => item.id === parseInt(id));
+
+    if (index !== -1) {
+      db.get(resource).splice(index, 1).write();
+      res.status(200).json({ message: "Data deleted successfully" });
+    } else {
+      res.status(404).json({ error: "Data not found" });
+    }
+  } else {
+    res.status(404).json({ error: "Resource not found" });
+  }
+});
 
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
